2872c0d036a747fba5be4bdaae03d2be9f7965a8
20141105052 <xiaoods3on1@sina.com>
2015-11-11 19:46:31 +0800

    第二次提交
    从小到大排序

--- paixu/main.cpp
+++ paixu/main.cpp
@@ -10,6 +10,28 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int i,j;
+    int t;
+    int a[10];
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10-i-1;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",a[i]);
+    }
     return 0;
 }
