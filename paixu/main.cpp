667144013f889e9221bbbc9ac131d98475489b0e
20141105052 <xiaoods3on1@sina.com>
2015-11-11 19:45:00 +0800

    Initial Commit

---
+++ paixu/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  paixu
+//
+//  Created by 20141105052 on 15/11/11.
+//  Copyright (c) 2015年 20141105052. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
